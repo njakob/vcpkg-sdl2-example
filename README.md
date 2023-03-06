a36734bd70a8a32ae0cacd7facb961c0cb9a5383
Nicolas Jakob <njakob@users.noreply.github.com>
2023-03-06 19:04:32 +0100

    Initial setup with SDL2 and Vcpkg

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vcpkg Example with SDL2
+
+This repository is a simple setup of [SDL2](https://github.com/libsdl-org/SDL) using [Vcpkg](https://vcpkg.io/) as a package manager.
+
+It includes the following Vcpkg ports:
+
+- [`sdl2`](https://vcpkg.link/ports/sdl2)
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+
+static auto shouldQuit = false;
+
+auto ProcessInputs() -> void {
+  SDL_Event event;
+  while (SDL_PollEvent(&event) > 0) {
+    switch (event.type) {
+      case SDL_QUIT: {
+        shouldQuit = true;
+        return;
+      }
+    }
+  }
+}
+
+auto main() -> int {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cout << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto window = SDL_CreateWindow("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+
+  while (!shouldQuit) {
+    ProcessInputs();
+  }
+
+  return EXIT_SUCCESS;
+}
